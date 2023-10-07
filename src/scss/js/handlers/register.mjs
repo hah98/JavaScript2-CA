dec298b870919096570c4a25a5615996c7921885
hah98 <74331454+hah98@users.noreply.github.com>
2023-10-07 04:35:28 +0200

    Added login and register html and JS files

---
+++ src/scss/js/handlers/register.mjs
@@ -0,0 +1,12 @@
+export function setRegisterFormListener() {
+  const form = document.querySelector("#registerForm");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const profile = Object.fromEntries(formData.entries());
+    console.log(profile);
+    console.log("it worked");
+  });
+}
