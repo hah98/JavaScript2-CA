14e8549e0881d28fc8d3570d066a5d84a48f4154
hah98 <74331454+hah98@users.noreply.github.com>
2023-10-13 23:59:28 +0200

    test index with api methods

--- src/scss/js/index.mjs
+++ src/scss/js/index.mjs
@@ -1,5 +1,11 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
+/* import { createPost } from "./api/posts/create.mjs"; */
+/* import { updatePost } from "./api/posts/update.mjs"; */
+import { removePost } from "./api/posts/delete.mjs";
+import * as post from "./api/posts/index.mjs";
+
+/* import * as posts from "./api/posts/index.mjs"; */
 
 const path = location.pathname;
 
@@ -8,3 +14,19 @@ if (path === "/profile/register/index.html") {
 } else if (path === "/profile/login/index.html") {
   setLoginFormListener();
 }
+
+
+// Testing worked yippy!!!!//
+/* removePost({
+  id: 6096,
+  title: "Example Post UPDATED",
+  body: "Also an example UPDATED",
+});
+ */
+/* removePost(6096); */
+
+/* post.createPost();
+post.updatePost();
+post.removePost();
+post.getPost(); */
+/* post.getPost(6099).then(console.log); */
