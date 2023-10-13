fecc69d1f1a74ac1a08c4fb5a90adfbdfd1b16ee
hah98 <74331454+hah98@users.noreply.github.com>
2023-10-13 23:52:40 +0200

    Added post api methods to project

---
+++ src/scss/js/api/posts/read.mjs
@@ -0,0 +1,33 @@
+/* export async function getPosts() {
+
+}
+
+export async function getpost(id){
+
+} */
+
+import { API_SOCIAL_URL } from "../constants.mjs";
+
+import { authFetch } from "../authFetch.mjs";
+
+const action = "/posts";
+/* const method = "delete"; */
+
+export async function getPosts() {
+  const getPostsUrl = `${API_SOCIAL_URL}${action}`;
+
+  const response = await authFetch(getPostsUrl);
+
+  return await response.json();
+}
+
+export async function getPost(id) {
+  if (!id) {
+    throw new Error("You need postID to GET");
+  }
+  const getPostUrl = `${API_SOCIAL_URL}${action}/${id}`;
+
+  const response = await authFetch(getPostUrl);
+
+  return await response.json();
+}
