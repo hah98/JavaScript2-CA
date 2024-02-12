a898b5690c59160bbf75d51663eabeae33a2a7a5
hah98 <74331454+hah98@users.noreply.github.com>
2024-02-12 20:53:11 +0100

    export file update with two new js files

--- src/scss/js/handlers/index.mjs
+++ src/scss/js/handlers/index.mjs
@@ -3,3 +3,5 @@ export * from "./login.mjs";
 export * from "./createPost.mjs";
 export * from "./updatePost.mjs";
 export * from "./updateProfile.mjs";
+export * from "./viewAllposts.mjs";
+export * from "./viewOnePost.mjs";
